e2df0412fefd2f19d00d9b0cb3d2f4c6f1734f49
Alim Haji <alim.haji@gmail.com>
2019-05-14 21:43:24 -0600

    Correct npm scripts

--- webpack.config.js
+++ webpack.config.js
@@ -4,45 +4,73 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const outputDirectory = 'dist';
 
-module.exports = {
-  entry: ['babel-polyfill', './src/client/app-client.js'],
-  output: {
-    path: path.join(__dirname, outputDirectory),
-    filename: 'bundle.js',
-    publicPath: '/',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+module.exports = [
+  {
+    entry: ['babel-polyfill', './src/client/app-client.js'],
+    output: {
+      path: path.join(__dirname, outputDirectory),
+      filename: 'bundle.js',
+      publicPath: '/',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
         },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.scss$/,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+      ],
+    },
+    devServer: {
+      port: 3000,
+      open: true,
+      proxy: {
+        '/api': 'http://localhost:8080',
       },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
+      historyApiFallback: true,
+    },
+    plugins: [
+      new CleanWebpackPlugin([outputDirectory]),
+      new HtmlWebpackPlugin({
+        template: './public/index.ejs',
+        historyApiFallback: true,
+      }),
     ],
   },
-  devServer: {
-    port: 3000,
-    open: true,
-    proxy: {
-      '/api': 'http://localhost:8080',
+  {
+    target: 'node',
+    node: {
+      __dirname: false,
     },
-    historyApiFallback: true,
+    externals: [/^(?!\.|\/).+/i],
+    entry: {
+      js: './src/server/server.js',
+    },
+    output: {
+      path: path.join(__dirname, outputDirectory),
+      filename: 'server-es5.js',
+      libraryTarget: 'commonjs2',
+    },
+    module: {
+      rules: [
+        {
+          test: path.join(__dirname, 'src'),
+          use: {
+            loader: 'babel-loader',
+            options: 'cacheDirectory=.babel_cache',
+          },
+        },
+      ],
+    },
   },
-  plugins: [
-    new CleanWebpackPlugin([outputDirectory]),
-    new HtmlWebpackPlugin({
-      template: './public/index.ejs',
-      historyApiFallback: true,
-    }),
-  ],
-};
+];
