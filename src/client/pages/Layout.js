5d75cc171cf10ece91362f21a5e3fe8e85a80626
Alim Haji <alim.haji@gmail.com>
2019-05-16 18:47:10 -0600

    Revert logo

--- src/client/pages/Layout.js
+++ src/client/pages/Layout.js
@@ -6,16 +6,7 @@ export const Layout = props => (
   <div className="app-container">
     <Navbar bg="light" variant="light">
       <LinkContainer to="/">
-        <Navbar.Brand>
-          <img
-            alt=""
-            src="logo.svg"
-            width="52"
-            height="52"
-            className="d-inline-block align-center"
-          />
-          &nbsp;&nbsp;GroupTrip!
-        </Navbar.Brand>
+        <Navbar.Brand>&nbsp;&nbsp;GroupTrip!</Navbar.Brand>
       </LinkContainer>
     </Navbar>
     <div className="app-content">{props.children}</div>
