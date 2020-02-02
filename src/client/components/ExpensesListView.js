3d362c076a92b7c9d97ae1d952c1e42de2120ce6
Alim Haji <alim.haji@gmail.com>
2020-02-02 14:09:26 +0000

    Add Show All expenses to tripIndex

--- src/client/components/ExpensesListView.js
+++ src/client/components/ExpensesListView.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -7,11 +8,10 @@ import Badge from 'react-bootstrap/Badge';
 
 import ListGroup from 'react-bootstrap/ListGroup';
 
-const formatMoney = a =>
-  '$ '.concat(a.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,'));
+const formatMoney = (a) => '$ '.concat(a.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,'));
 
 export const ExpensesListView = (props) => {
-  const { expenses, onEdit } = props;
+  const { expenses, onEdit, tripId } = props;
 
   const rows = expenses.map((exp, n) => (
     <ListGroup.Item key={exp.id} active={exp.active || false}>
@@ -27,11 +27,13 @@ export const ExpensesListView = (props) => {
             </h4>
           </Col>
           <Col>
-            {exp.canEdit && exp.needsAttention ? (
-              <Badge variant="warning">Incomplete</Badge>
-            ) : (
-              <div />
-            )}
+            {exp.canEdit && exp.needsAttention
+              ? (
+                <Badge variant="warning">Incomplete</Badge>
+              )
+              : (
+                <div />
+              )}
           </Col>
         </Row>
         <Row>
@@ -54,7 +56,11 @@ export const ExpensesListView = (props) => {
 
   const rowsWithAdd = [
     rows,
-    <ListGroup.Item key="show-all">...</ListGroup.Item>,
+    <ListGroup.Item key="show-all" style={{ textAlign: 'center' }}>
+      <Button variant="link">
+        <Link to={'/trips/'.concat(tripId).concat('/expenses')}>Show All</Link>
+      </Button>
+    </ListGroup.Item>,
   ];
   return <ListGroup>{rowsWithAdd}</ListGroup>;
 };
